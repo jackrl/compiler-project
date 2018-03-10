f908fed3872f1cef16da2d68b87bbd95f92d0442
Jacinto Ramirez Lahti <jacinto.ramirez.lahti@cs.helsinki.fi>
2018-03-11 01:15:41 +0200

    Added prompt to be able to test the iterpreter easily

--- MiniPlInterpreter/Program.cs
+++ MiniPlInterpreter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MiniPlInterpreter
 {
@@ -13,12 +14,48 @@ namespace MiniPlInterpreter
             else if (args.Length == 1)
             {
                 Console.WriteLine("Running script: " + args[0]);
-                //runFile(args[0]);
+                RunFile(args[0]);
             }
             else
             {
                 Console.WriteLine("Starting prompt");
-                //runPrompt();
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(String path)
+        {
+            try
+            {
+                string source = System.IO.File.ReadAllText(path);
+                Run(source);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Couldn't read file: {path}");
+            }
+            
+        }
+
+        private static void RunPrompt()
+        {
+            while (true) { 
+                Console.Write("> ");
+                var input = Console.ReadLine();
+                if (input.Equals("quit")) break;
+                Run(input);
+            }
+        }
+
+private static void Run(String source)
+        {
+            Scanner scanner = new Scanner(source);
+            List<Token> tokens = scanner.ScanTokens();
+
+            // For now, just print the tokens.
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token);
             }
         }
     }
