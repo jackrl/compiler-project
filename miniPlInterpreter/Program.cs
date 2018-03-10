178a0419f3461e6eb773d02522701f7314f8b6f6
jackrl <jacinto.ramirez.lahti@cs.helsinki.fi>
2018-03-10 20:48:46 +0200

    Project created

---
+++ miniPlInterpreter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace miniPlInterpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
